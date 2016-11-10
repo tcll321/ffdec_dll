7084d9904aaf51b202ef9dac59e60db92ce55bfe
tcll321 <tcll321@126.com>
2016-11-10 20:52:36 +0800

    11.20

--- ffdec/src/ffdec.cpp
+++ ffdec/src/ffdec.cpp
@@ -3,13 +3,27 @@
 
 #include "stdafx.h"
 #include "ffdec.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
 #include "avcodec.h"
+extern AVCodec h264_decoder;
+
+#ifdef __cplusplus
+};
+#endif
 
 // This is an example of an exported function.
 FFDEC_API int np_ffdec_init(NPFFDEC_ID id)
 {
-	avcodec_register_all();
+	register_avcodec(&h264_decoder);
 	avcodec_init();
-	avcodec_find_decoder(id);
+
+	CodecID ffID = CODEC_ID_NONE;
+	if (FFDEC_ID_H264 == id)
+		ffID = CODEC_ID_H264;
+
+	avcodec_find_decoder(ffID);
 	return 42;
 }
